0b7743f82b541e6a729c67587d8a8c86fd9009a1
sadek al mostakim <s.a.mostakim@gmail.com>
2018-08-08 02:36:31 +0600

    152

--- app/main.py
+++ app/main.py
@@ -1,13 +1,4 @@
-import pickle
-from pandas.core import reshape
 from flask import Flask
-from flask import request
-from flask import jsonify
-from sklearn.externals import joblib
-import numpy as np
-import pandas as pd
-from pandas import DataFrame
-
 
 app = Flask(__name__)
 
@@ -15,13 +6,13 @@ app = Flask(__name__)
 def home():
     return "Welcome"
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    if request.method == 'POST':  
-        classes = ['technology', 'economy', 'entertainment', 'international', 'sports']      
-        data = request.get_json()      
-        lin_reg = joblib.load("LinearSVC_modelAugust.pkl")
-        result =lin_reg.predict(data).tolist() 
-        return jsonify(classes[result[0]])
+# @app.route("/predict", methods=['POST'])
+# def predict():
+    # if request.method == 'POST':  
+    #     classes = ['technology', 'economy', 'entertainment', 'international', 'sports']      
+    #     data = request.get_json()      
+    #     lin_reg = joblib.load("LinearSVC_modelAugust.pkl")
+    #     result =lin_reg.predict(data).tolist() 
+    #     return jsonify(classes[result[0]])
 if __name__ == '__main__':
   app.run()
